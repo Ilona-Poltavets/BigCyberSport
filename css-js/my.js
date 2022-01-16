2886d12b6fba22e3712e064b97d2700a05638ede
marina.kotova <20ilona.poltavets02@gmail.com>
2022-01-16 15:29:09 +0300

    add material designer

--- css-js/my.js
+++ css-js/my.js
@@ -1,25 +1,25 @@
 $('document').ready(function () {
     var rowText;
-    var content=$('#teams');
-    const teams=getTeams();
-    for(let row of teams){
-        rowText=
+    var content = $('#teams');
+    const teams = getTeams();
+    for (let row of teams) {
+        rowText =
             `<tr>
                 <td>${row.name}</td>
-                <td class="text-right">
-                    <a class="button primary" href="team.html?name=${row.name}">
-                        <i class="fi fi-pencil"></i>
+                <td class="right-align">
+                    <a class="btn-floating waves-effect waves-light blue" href="team.html?name=${row.name}">
+                        <i class="material-icons">edit</i>
                     </a>
-                    <button type="button" class="button alert" rowid="${row.name}">
-                        <i class="fi fi-trash"></i>
-                    </button>
+                    <a rowid="${row.name}" class="rem-row btn-floating waves-effect waves-light red">
+                        <i class="material-icons">delete</i>
+                    </a>
                 </td>
                </tr>`;
         content.append(rowText);
     }
-    $('.rem-row').click(function(){
-        let name=$(this).attr('rowid');
-        saveTeams(teams.filter((t)=>t.name!=name));
+    $('.rem-row').click(function () {
+        let name = $(this).attr('rowid');
+        saveTeams(teams.filter((t) => t.name != name));
         location.reload();
     })
 })
